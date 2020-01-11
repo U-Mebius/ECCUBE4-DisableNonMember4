5b420df5ebe4ae49ba2c492da9dff9b13b5a6ccf
dakeyama <dakeyama@umebius.com>
2020-01-11 15:30:30 +0900

    PHPDoc メッセージ修正

--- Service/PurchaseFlow/Processor/MemberValidator.php
+++ Service/PurchaseFlow/Processor/MemberValidator.php
@@ -1,56 +1,22 @@
 <?php
 
-/*
- * This file is part of EC-CUBE
- *
- * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
- *
- * http://www.ec-cube.co.jp/
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Plugin\DisableNonMember4\Service\PurchaseFlow\Processor;
 
-use Eccube\Annotation\CartFlow;
-use Eccube\Annotation\OrderFlow;
 use Eccube\Annotation\ShoppingFlow;
 use Eccube\Common\EccubeConfig;
 use Eccube\Entity\ItemHolderInterface;
-use Eccube\Entity\ItemInterface;
 use Eccube\Entity\Order;
 use Eccube\Service\PurchaseFlow\InvalidItemException;
-use Eccube\Service\PurchaseFlow\ItemHolderPostValidator;
 use Eccube\Service\PurchaseFlow\ItemHolderValidator;
 use Eccube\Service\PurchaseFlow\PurchaseContext;
 use Eccube\Service\PurchaseFlow\ItemValidator;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
- * 商品を１個のみしか購入できないようにするサンプル
- *
- * # 使い方
- * PurchaseFlowに新しいProcessorを追加する
- *
- * ## 追加できるプロセッサ
- * 以下のクラスまたはインタフェースを継承または実装している必要がある
- * * ItemPreprocessor
- * * ItemValidator
- * * ItemHolderPreprocessor
- * * ItemHolderValidator
- * * PurchaseProcessor
- *
- * ## 追加対象のフローの指定方法
- * * カートのPurchaseFlowにProcessorを追加する場合はCartFlowアノテーションを追加
- * * 購入フローのPurchaseFlowにProcessorを追加する場合はShoppingFlowアノテーションを追加
- * * 管理画面でのPurchaseFlowにProcessorを追加する場合はOrderFlowアノテーションを追加
- *
  * @ShoppingFlow
  */
 class MemberValidator extends ItemHolderValidator
 {
-
     /**
      * @var RequestStack
      */
@@ -85,7 +51,6 @@ class MemberValidator extends ItemHolderValidator
         /* @var $Order Order */
         $Order = $itemHolder;
 
-        // 受注の生成直後はチェックしない.
         if (!$Order->getCustomer()) {
             $this->throwInvalidItemException('please.login.to.purchase', null, false);
         }
